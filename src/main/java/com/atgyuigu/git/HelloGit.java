12f4d47c9b15480fd09bef1b8c615fef20fa5926
fyh <fyh1996_man@qq.com>
2022-04-19 06:11:26 +0800

    dev 分支 创建 helloGit类

---
+++ src/main/java/com/atgyuigu/git/HelloGit.java
@@ -0,0 +1,16 @@
+package com.atgyuigu.git;
+
+
+
+/**
+ * @ClassName: HelloGit
+ * @Description:
+ * @Author: 冯永辉
+ * @Date: 2022/4/19 6:09
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        String name ="git";
+        System.out.println(name);
+    }
+}
