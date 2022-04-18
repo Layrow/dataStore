dec2bb764cbfb9e27b446834e8731bcbfda500d0
fyh <fyh1996_man@qq.com>
2022-04-19 06:54:03 +0800

    哎为啥那么么满呢操

---
+++ src/main/java/com/atgyuigu/git/NewLa.java
@@ -0,0 +1,13 @@
+package com.atgyuigu.git;
+
+/**
+ * @ClassName: NewLa
+ * @Description:
+ * @Author: 冯永辉
+ * @Date: 2022/4/19 6:53
+ */
+public class NewLa {
+    public static void main(String[] args) {
+        System.out.println("为啥那么慢呢你妈的");
+    }
+}
