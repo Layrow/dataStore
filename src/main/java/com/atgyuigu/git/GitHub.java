c8243e1e3f4962987d3abb4dcae3f84cfff29a36
fyh <fyh1996_man@qq.com>
2022-04-19 06:25:42 +0800

    github add dev baranch

---
+++ src/main/java/com/atgyuigu/git/GitHub.java
@@ -0,0 +1,13 @@
+package com.atgyuigu.git;
+
+/**
+ * @ClassName: GitHub
+ * @Description:
+ * @Author: 冯永辉
+ * @Date: 2022/4/19 6:24
+ */
+public class GitHub {
+    public static void main(String[] args) {
+        System.out.println("add dev branch");
+    }
+}
