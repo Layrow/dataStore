8ca1d4c8c303b09f3e17026599fe526165e1c8a5
fyh <fyh1996_man@qq.com>
2022-04-19 06:08:36 +0800

    输出age

---
+++ src/main/java/com/atgyuigu/git/TestGit.java
@@ -0,0 +1,14 @@
+package com.atgyuigu.git;
+
+/**
+ * @ClassName: TestGit
+ * @Description:
+ * @Author: 冯永辉
+ * @Date: 2022/4/19 5:59
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        int age = 15;
+        System.out.println(age);
+    }
+}
