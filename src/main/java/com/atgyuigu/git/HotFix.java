6e267c69c1b91f8e726718151984dc5f2378f9dc
fyh <fyh1996_man@qq.com>
2022-04-19 06:46:58 +0800

    新建hot-fix分支

---
+++ src/main/java/com/atgyuigu/git/HotFix.java
@@ -0,0 +1,13 @@
+package com.atgyuigu.git;
+
+/**
+ * @ClassName: HotFix
+ * @Description:
+ * @Author: 冯永辉
+ * @Date: 2022/4/19 6:46
+ */
+public class HotFix {
+    public static void main(String[] args) {
+        System.out.println("hotdev");
+    }
+}
