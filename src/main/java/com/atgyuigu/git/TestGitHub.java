bb045111e8f4c4bb02e5014e52d8248220991fc4
fyh <fyh1996_man@qq.com>
2022-04-19 07:19:17 +0800

    修改了

--- src/main/java/com/atgyuigu/git/TestGitHub.java
+++ src/main/java/com/atgyuigu/git/TestGitHub.java
@@ -1,5 +1,13 @@
-public Class TestGitHub{
+package com.atgyuigu.git;
+/**
+* @description: 
+* @param null	
+* @return: 
+* @author: 冯永辉
+* @time: 2022/4/19 7:18
+*/ 
+public class TestGitHub{
   public static void main(String[] args){
-    System.out.println("hello word")
+    System.out.println("hello word");
   }
 }
